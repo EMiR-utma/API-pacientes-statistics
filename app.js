380ddc3edfec76d7d020c4bc4c92abbc93fe9511
Emilio <utm22030622@utma.edu.mx>
2026-03-26 22:12:13 -0600

    Re: Environment variable used and cleaning

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); 
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 const cors = require('cors');
 const { pool } = require('./db'); 
 
